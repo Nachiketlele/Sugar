502f4d5164a272a0c27a51bf7b4693cc7211d91b
Rahul Rajeevan <rahul.rajeevan4@gmail.com>
2022-06-16 15:30:58 +0530

    functionality working on all pages

--- src/pages/TRENDING.js
+++ src/pages/TRENDING.js
@@ -1,12 +1,15 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Box } from '../context/context';
 import styles from './Style.module.css'
 import img1 from "./star_filled.png"
+import { useDispatch } from 'react-redux';
 
 
 const TRENDING = () => {
 const {time,setsortval}=useContext(Box);
 const [love, setlove] = useState(false)
+const dispatch=useDispatch()
+
 const handle=(e)=>{
 setsortval(e.target.value);
 }
@@ -32,6 +35,10 @@ const thelove=(val)=>{
 }
 
   time(list1)
+
+  const handleadd=(id)=>{
+      dispatch({ type: "ADD_TO_CART", payload:id })
+  }
 
  
 
@@ -61,7 +68,7 @@ const thelove=(val)=>{
         {list1.map(e=>(
         <div className={styles.q6} key={e.id}>
         <div className={styles.q2}>    <img src={e.src} alt="no"/>  <p>{e.name}</p>  <p>Rs:{e.price}</p> <div className={styles.q8} >  <img src={img1} alt='no' id={styles.e1}/>   <p>{e.rating}</p>  <img id={styles.e1} src={!e.love?"https://th.bing.com/th/id/R.5365673f66386551fa6df965535bcfa1?rik=UzbBo%2bjcduyTuQ&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fKTn%2fXnM%2fKTnXnMAgc.png&ehk=vhGmsnENrQL2IH%2f9jIOSj8tJvVOxXNRhYqiiQMzUQGw%3d&risl=&pid=ImgRaw&r=0":"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png"} alt='no' onClick={()=>thelove(e.id)}/></div> 
-        <div className={styles.q7}>ADD TO CART </div>
+        <div className={styles.q7} onClick={()=>handleadd(e.id)}>ADD TO CART </div>
         </div>
         </div>))}
     </div>
